cdc4352be0e31ff9733895b117266444c98a14de
Courtney Brown <clbrown0607@gmail.com>
2021-07-12 18:21:51 -0500

    updated Nav component

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -3,16 +3,9 @@ import { capitalizeFirstLetter } from '../../utils/helpers';
 
 function Nav(props) {
   const {
-    categories = [],
-    setCurrentCategory,
-    contactSelected,
-    currentCategory,
-    setContactSelected,
+    currentTab,
+    setCurrentTab
   } = props;
-
-  useEffect(() => {
-    document.title = capitalizeFirstLetter(currentCategory.name);
-  }, [currentCategory]);
 
   return (
     <header className="flex-row px-1">
@@ -23,37 +16,26 @@ function Nav(props) {
       </h2>
       <nav>
         <ul className="flex-row">
-          <li className="mx-2">
-            <a data-testid="about" href="#about" onClick={() => setContactSelected(false)}>
+          <li className={currentTab === "about" ? "mx-2 navActive" : "mx-2"}>
+            <span onClick={() => setCurrentTab("about")}>
               About me
-            </a>
+            </span>
           </li>
-          <li className={`mx-2 ${portfolioSelected && 'navActive'}`}>
-            <span onClick={() => setPortfolioSelected(true)}>Portfolio</span>
+          <li className={currentTab === "portfolio" ? "mx-2 navActive" : "mx-2"}>
+            <span onClick={() => setCurrentTab("portfolio")}>
+              Portfolio
+            </span>
           </li>
-          <li className={`mx-2 ${contactSelected && 'navActive'}`}>
-            <span onClick={() => setContactSelected(true)}>Contact</span>
+          <li className={currentTab === "contact" ? "mx-2 navActive" : "mx-2"}>
+            <span onClick={() => setCurrentTab("contact")}>
+              Contact
+            </span>
           </li>
-          <li className={`mx-2 ${resumeSelected && 'navActive'}`}>
-            <span onClick={() => setResumeSelected(true)}>Resume</span>
+          <li className={currentTab === "resume" ? "mx-2 navActive" : "mx-2"}>
+            <span onClick={() => setCurrentTab("resume")}>
+              Resumé
+            </span>
           </li>
-          {categories.map((category) => (
-            <li
-              className={`mx-1 ${
-                currentCategory.name === category.name && !contactSelected && 'navActive'
-                }`}
-              key={category.name}
-            >
-              <span
-                onClick={() => {
-                  setCurrentCategory(category);
-                  setContactSelected(false);
-                }}
-              >
-                {capitalizeFirstLetter(category.name)}
-              </span>
-            </li>
-          ))}
         </ul>
       </nav>
     </header>
